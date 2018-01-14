4196248e020b0b4de75c0b900f7ab42842f78dd4
mahbub02 <sadiashormin@gmail.com>
2018-01-14 11:17:22 -0600

    fix searchForm to show index with title

--- src/form/formResults.js
+++ src/form/formResults.js
@@ -20,7 +20,7 @@ export class FormResults extends React.Component {
         {this.props.results.map((element, index) => {
           return(
             <li key={index} onClick={this.handleClick}>
-              <Link to={`/movie/${this.props.results[index].id}`} >
+              <Link to={`/movie/${encodeURIComponent(this.props.results[index].title)}`} >
                 <img src={this.props.results[index].poster_path === null ? '../archive/640.png' : `${link}${this.props.results[index].poster_path}`} alt={`${this.props.results[index].title} poster`} className="resultPoster" />
                 <div>
                   <p>{this.props.results[index].title}</p>
