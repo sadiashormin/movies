3e49553ce936bed758a3124f84e8df2375b65c97
mahbub02 <mahbub.rahman0204@gmail.com>
2018-01-14 23:25:44 -0600

    Update mapmac.sh

--- mapmac.sh
+++ mapmac.sh
@@ -9,11 +9,10 @@ else
 fi
 
 
-
-
-if grep -q 'b8:e8:56:32:8c:9a' /etc/mapmac.info ##note the space after the string you are searching for
+result=$( ifconfig en0 | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}' )
+if grep -q $result /etc/mapmac.info 
 then
-echo "Success !! This card is mapped to this device"
+    echo "Success !! This card is mapped to this device" 
 else
-echo "Error !! This card is not mapped to this device"
+    echo "Error !! This card is not mapped to this device"
 fi
